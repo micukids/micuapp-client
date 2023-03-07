c70d4bc78639fc6c545a9f9bd872a8a6828b005c
Elvia Benedith <ejbenedith@gmail.com>
2023-03-08 00:16:00 +0100

    footer desing ready

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,28 +2,54 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Logo_Horizontal from '../../assets/img/Logo_Micukids_horizontal.png';
+import micuKidsLogo_Horizontal from '../../assets/img/Logo_Micukids_horizontal.png';
+import facebook_logo from '../../assets/img/🦆 icon _circle facebook_.svg';
+import instagram_logo from '../../assets/img/🦆 icon _rounded instagram_.svg';
 import './Footer.css'
  
 const Footer = () => {
   return (
-    <Navbar bg="dark" >
+    <Navbar bg="dark" fixed="bottom" >
     <Container>
       <Navbar.Brand href="#">
         <img
-          src={Logo_Horizontal}
-          width="150"
-          height="40"
+          src={micuKidsLogo_Horizontal}
+          width="140"
+          height="42"
           className="d-inline-block align-top"
           alt="micuKids logo"
         />
       </Navbar.Brand>
+      <Nav
+            className="justify-content-center"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">        
+              <img
+                src={facebook_logo}
+                width="41.46"
+                height="41.46"
+                className="d-inline-block align-top"
+                alt="micuKids logo"
+              />
+           </Nav.Link>
+            <Nav.Link href="#action1">        
+              <img
+                src={instagram_logo}
+                width="41.46"
+                height="41.46"
+                className="d-inline-block align-top"
+                alt="micuKids logo"
+              />
+           </Nav.Link>  
+          </Nav>
       <Nav>
             <Nav.Link href="#deets">CRÉDITOS</Nav.Link>
             <Nav.Link eventKey={2} href="#memes">
               CONTACTO
             </Nav.Link>
-          </Nav>
+      </Nav>
     </Container>
   </Navbar>
   )
--- src/pages/home/App.jsx
+++ src/pages/home/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Footer from "../../components/footer/Footer";
 
 function App() {
   return (
     <div className="App">
       Home
-      <Footer/>
     </div>
   );
 }
