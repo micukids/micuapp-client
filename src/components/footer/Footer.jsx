69c7f13520dd4448a7c28d744436ee6d8b0bc5b8
Elvia Benedith <ejbenedith@gmail.com>
2023-03-07 20:56:27 +0100

    footer first commit

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Logo_Horizontal from '../../assets/img/Logo_Micukids_horizontal.png';
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <Navbar bg="dark">
+    <Container>
+      <Navbar.Brand href="#home">
+        <img
+          src={Logo_Horizontal}
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+          alt="React Bootstrap logo"
+        />
+      </Navbar.Brand>
+    </Container>
+  </Navbar>
   )
 }
 
