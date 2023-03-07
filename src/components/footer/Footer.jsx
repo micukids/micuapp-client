e4a538de00e84c257509a37ce38a7392edac7ccb
Elvia Benedith <ejbenedith@gmail.com>
2023-03-07 22:23:06 +0100

    footer styles

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,21 +1,29 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Logo_Horizontal from '../../assets/img/Logo_Micukids_horizontal.png';
-
+import './Footer.css'
+ 
 const Footer = () => {
   return (
-    <Navbar bg="dark">
+    <Navbar bg="dark" >
     <Container>
-      <Navbar.Brand href="#home">
+      <Navbar.Brand href="#">
         <img
           src={Logo_Horizontal}
-          width="30"
-          height="30"
+          width="150"
+          height="40"
           className="d-inline-block align-top"
-          alt="React Bootstrap logo"
+          alt="micuKids logo"
         />
       </Navbar.Brand>
+      <Nav>
+            <Nav.Link href="#deets">CRÉDITOS</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              CONTACTO
+            </Nav.Link>
+          </Nav>
     </Container>
   </Navbar>
   )
--- src/pages/home/App.jsx
+++ src/pages/home/App.jsx
@@ -1,8 +1,12 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from "../../components/footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      
+      Home
+      <Footer/>
     </div>
   );
 }
