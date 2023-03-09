87263de86fb3b1cc1331219e0376bd0fe317c9e0
KaroVillarraga <karovilla@gmail.com>
2023-03-09 15:06:50 +0100

    logo Marta into assets

--- src/components/memory/Memory.jsx
+++ src/components/memory/Memory.jsx
@@ -7,6 +7,8 @@ const Memory = () => {
   return (
     <div className="Memory">
     <h1>Memory cards</h1>
+    <h2>con la voz de</h2>
+    <img src="../../assets/img/Logo_Marta.png" alt="Marta Gómez" />
     <MainButton class='bt' text='¡Vamos a jugar memory!'/>
     </div>
   );
