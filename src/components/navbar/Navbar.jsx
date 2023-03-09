c82f114c2d3f5294bc2c29a38f6b51175ae86fab
Elvia Benedith <ejbenedith@gmail.com>
2023-03-09 16:46:08 +0100

    logo refactorized

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Micu_cuadrado from '../../assets/img/Logo_Micu_cuadrado.png'
-import '../footer/Footer.css'
+import Logo from '../logo/Logo';
+
 
 const NavBar = () => {
   return (
@@ -11,13 +11,7 @@ const NavBar = () => {
 <Navbar bg="dark" fixed="top" >
     <Container>
       <Navbar.Brand href="#">
-        <img
-          src={Micu_cuadrado}
-          width="140"
-          height="42"
-          className="d-inline-block align-top"
-          alt="micuKids logo"
-        />
+       <Logo/>
       </Navbar.Brand>
       <Nav>
             <Nav.Link href="#deets">REGISTER</Nav.Link>
