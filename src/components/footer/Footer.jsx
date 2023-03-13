df328d8fd17daaed966beb5b0885d0ef12819149
Elvia Benedith <ejbenedith@gmail.com>
2023-03-13 19:55:36 +0100

    youtube added to footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,6 +5,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import micuKidsLogo_Horizontal from '../../assets/img/Logo_Micukids_horizontal.png';
 import facebook_logo from '../../assets/img/🦆 icon _circle facebook_.svg';
 import instagram_logo from '../../assets/img/🦆 icon _rounded instagram_.svg';
+import youtube_logo from '../../assets/img/icon_Youtube.svg'
 import './Footer.css'
  
 const Footer = () => {
@@ -31,29 +32,37 @@ const Footer = () => {
                 width="41.46"
                 height="41.46"
                 className="d-inline-block align-top"
-                alt="micuKids logo"
+                alt="facebook logo"
               />
            </Nav.Link>
+              <Nav.Link href="https://www.youtube.com/@MicuKids" target="_blank">        
+              <img
+                src={youtube_logo}
+                width="41.46"
+                height="41.46"
+                className="d-inline-block align-top"
+                alt="youtube logo"
+              />
+           </Nav.Link>  
             <Nav.Link href="https://www.instagram.com/micukids/" target="_blank">        
               <img
                 src={instagram_logo}
                 width="41.46"
                 height="41.46"
                 className="d-inline-block align-top"
-                alt="micuKids logo"
+                alt="instagram logo"
               />
-           </Nav.Link>  
+            </Nav.Link> 
           </Nav>
       <Nav>
-            <Nav.Link 
-            href="#deets">
+            <Nav.Link href="/creditos">
               CRÉDITOS
-              </Nav.Link>
-            <Nav.Link href="#memes">
+            </Nav.Link>
+            <Nav.Link href="/contacto">
               CONTACTO
             </Nav.Link>
-            <Nav.Link href="#memes">
-               ACCESO
+            <Nav.Link href="/login">
+               ACCESO ADMIN
             </Nav.Link>
       </Nav>
     </Container>
