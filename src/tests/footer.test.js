f2d448cd405ef008677467657c6bb4212db54fd5
LilianaDal <116796494+LilianaDal@users.noreply.github.com>
2023-03-21 12:45:40 +0100

    Eight tests  done

--- src/tests/footer.test.js
+++ src/tests/footer.test.js
@@ -9,5 +9,21 @@ describe('Footer', () => {
         expect(elements).toBeInTheDocument();
     })
 
-    //Describir más tests aqui//
+    test("Texto contacto existe dentro del documento", () => {
+        render(<Footer/>);
+        expect(screen.getByText(/CONTACTO/i)).toBeInTheDocument();
+    })
+
+    test("Texto creditos existe dentro del documento", () => {
+        render(<Footer/>);
+        expect(screen.getByText(/CRÉDITOS/i)).toBeInTheDocument();
+    })
+
+    test("elemento ancor existe dentro del documento", () => {
+        const { container } = render(<Footer />)
+        const elements = container.querySelector('a')
+        expect(elements).toBeInTheDocument();
+
+    })
+
 })
