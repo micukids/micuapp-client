0dd67918ed6f6c5d734a5efaa55e7a029e4e58d9
Elvia Benedith <ejbenedith@gmail.com>
2023-03-28 00:17:07 +0200

    downloads secction created

--- src/components/admin/Downloads.jsx
+++ src/components/admin/Downloads.jsx
@@ -1,8 +1,60 @@
 import React from 'react'
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import { Link } from 'react-router-dom';
 
 function Downloads() {
   return (
-    <div>Downloads</div>
+    <div className='container-fluid px-4'>
+    <h1 className='mt-4'>Descargables</h1>
+
+    <Tabs
+  defaultActiveKey="vista"
+  id="uncontrolled-tab-example"
+  className="mb-3"
+>
+  <Tab eventKey="vista" title="Vista">
+  <table className='table table-striped'>
+                <thead className='dash-table'>
+                    <tr>
+                        <th>Letra</th>
+                        <th>Tipo</th>
+                        <th>Sonido</th>
+                        <th>Imagen</th>
+                        <th>Video</th>
+                    </tr>
+                </thead>
+                <tbody>
+              
+                        <tr key='' >
+                            <td>  </td>    
+                            <td></td>  
+                            <td>
+                                <audio controls>
+                                    <source src='' type="audio/mpeg"/>
+                                </audio>
+                            </td>   
+                            <td> <img src='' alt={`Imagen letra `} width="170" height="170" /> </td>    
+                            <td>
+                                <video width="170" height="170" controls>
+                                    <source src='' type="video/mp4"/>
+                                </video>
+                            </td>
+                            <td className='buttons-table'>
+                                <Link to='' className='btn btn-warning'>Editar</Link>
+                                <button  className='btn btn-danger'>Eliminar</button>
+                            </td>
+                        </tr>
+           
+                </tbody>
+            </table>
+  </Tab>
+  <Tab eventKey="añadir" title="Añadir Letra">
+    gdsgfg
+  </Tab>
+</Tabs>
+</div>
+
   )
 }
 
