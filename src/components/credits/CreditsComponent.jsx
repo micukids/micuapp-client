d9a7c805aedbcc5e92eabe6cf6a8be628e525714
IsabelGuMo <gm.isabel@gmail.com>
2023-03-28 10:42:16 +0200

    Fix styles credits component

--- src/components/credits/CreditsComponent.jsx
+++ src/components/credits/CreditsComponent.jsx
@@ -2,21 +2,24 @@ import React from 'react'
 import star_2 from '../../assets/img/Star_2.png';
 import f5 from '../../assets/img/f5.png';
 import google from '../../assets/img/google.png';
+import '../../styles/Styles.css';
 
 
 const CreditsComponent = () => {
   return (
-    <div className="row form-container">
+    <div className="row credits-container">
       <div className="form-container-title d-flex justify-content-center align-items-center">
         <img className="star-icon" src={star_2} alt="Estrella de color amarillo"/>
         <p className="form-title">CRÉDITOS</p>
       </div>
-      <div className="d-flex">
+      <div className="d-flex col-10">
         <p className="description">Esta web fue desarrollada como proyecto final del Bootcamp de Desarrollo Web Full-Stack de Factoria F5 - Patrocinado por Google.org </p>
-        <img className="star-icon" src={f5} alt="Logo Factoría F5"/>
-        <img className="star-icon" src={google} alt="Logo google"/>
+        <div className="icon-container d-flex">
+          <img className="icon" src={f5} alt="Logo Factoría F5"/>
+          <img className="icon" src={google} alt="Logo google"/>
+        </div>
       </div>
-      <div>
+      <div className="credits-sections">
         <h6>DESARROLLADORAS</h6>
           <p>Elvia Benedith</p>
           <p>Gabriela Barajas</p>
@@ -24,36 +27,36 @@ const CreditsComponent = () => {
           <p>Karolina Villarraga</p>
           <p>Isabel Gutiérrez</p>
       </div>
-      <div>
+      <div className="credits-sections">
         <h6>DISEÑO UX/UI</h6>
         <p>Karolina Villarraga</p>
       </div>
-      <div>
+      <div className="credits-sections">
         <h6>VOZ</h6>
         <p>Marta Gómez</p>
       </div>
-      <div>
+      <div className="credits-sections">
         <h6>FORMADORAS</h6>
         <p>Alexandra Galarza</p>
         <p>Laura Rodríguez</p>
       </div>
-      <div>
+      <div className="credits-sections">
         <h6>RESPONSABLE DE PROMOCIÓN</h6>
         <p>Lola Martínez Cueto</p>
       </div>
-      <div>
+      <div className="credits-sections">
         <h6>STAKEHOLDER</h6>
         <p>Sandra Merino</p>
       </div>
-      <div>
+      <div className="credits-sections">
         <h6>SCRUM MASTER</h6>
         <p>Liliana Dalmarco</p>
       </div>
-      <div>
+      <div className="credits-sections">
         <h6>PRODUCT OWNER</h6>
         <p>Karolina Villarraga</p>
       </div>
-      <div>
+      <div className="credits-sections">
         <h6>CANCIÓN Y VÍDEO ANIMADO A, B, C, CH, D</h6>
         <p>Letra y música: Marta Gómez (SGAE)</p>
         <p>Marimba y percusión vocal: Marta Gómez</p>
