d9c6cfbe448bb313fc0a6c448167a2357a414105
KaroVillarraga <karovilla@gmail.com>
2023-03-29 23:35:16 +0200

    download login

--- src/components/download/Download.jsx
+++ src/components/download/Download.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 function Download({thumb, url, description}) {
+  let imageLink="";
+  if (localStorage.getItem('auth_token')){
+    imageLink=( <Link to={url} download target="_blank"><img src={thumb} alt={description} className='downloadShape mb-4'/></Link>)
+  }
+  else {
+    imageLink=( <Link to="/login"><img src={thumb} alt={description} className='downloadShape mb-4'/></Link>)
+  }
+
   return (
     <div className='d-flex flex-column align-items-center' >
-        <Link to={url} download target="_blank"><img src={thumb} alt={description} className='downloadShape mb-4'/></Link>
+        {imageLink}
         <p className='color-paragraph'>{description}</p>
     </div>
   )
